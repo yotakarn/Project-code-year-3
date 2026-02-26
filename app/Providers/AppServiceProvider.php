7204eb7ad17beba527409f43061ba73ea21552d1
yotakarn <yotakarn6677@gmail.com>
2026-02-27 00:13:51 +0700

    edit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,5 +24,8 @@ class AppServiceProvider extends ServiceProvider
             ::defaultView('vendor.pagination.default');
         \Illuminate\Pagination\Paginator
             ::defaultSimpleView('vendor.pagination.simple-default');
+        if (config('app.env') !== 'local') {
+        \Illuminate\Support\Facades\URL::forceScheme('https');
+    }
     }
 }
